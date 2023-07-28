5e6e2f5a43fba2d4e2acb6c16bb0060851caec45
xn--khngctn2302-sbb9ik <61654742+khongcoten2302@users.noreply.github.com>
2023-07-29 03:40:26 +0700

    Add files via upload

---
+++ JSON Editor /src/components/json-editor/utils/file.tsx
@@ -0,0 +1,35 @@
+import { v4 as uuid } from "uuid";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const downloadJsonFile = async (jsonString: string) => {
+  const fileName = uuid();
+  // Set the HREF to a Blob representation of the data to be downloaded
+  const blob = new Blob([jsonString], { type: "application/json" });
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    // for IE
+    window.navigator.msSaveOrOpenBlob(blob, fileName);
+  } else {
+    // for Non-IE (chrome, firefox etc.)
+
+    try {
+      // create an invisible A element
+      const link = document.createElement("a");
+      link.style.display = "none";
+      document.body.appendChild(link);
+
+      const href = await window.URL.createObjectURL(blob);
+      // blob ready, download it
+      link.href = href;
+      link.download = `${fileName}.json`;
+
+      // trigger the download by simulating click
+      link.click();
+
+      // cleanup
+      window.document.body.removeChild(link);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log("Error: fail to download a file.");
+    }
+  }
+};
