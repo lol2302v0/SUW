db96180565a7466a79d4ff80615f9f9181fcaa8f
xn--khngctn2302-sbb9ik <61654742+khongcoten2302@users.noreply.github.com>
2023-07-29 03:37:58 +0700

    Add files via upload

---
+++ JSON Editor /src/components/json-editor/file.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import { validFormattedJsonInput } from "../../mock-data";
+import { downloadJsonFile } from "../file";
+
+interface MockButtonProps {
+  onClick: () => void;
+}
+const MockButton: React.FC<MockButtonProps> = ({ onClick }) => (
+  <button type="button" data-testid="mock-button" aria-label="download" onClick={onClick} />
+);
+
+describe("downloadJsonFile", () => {
+  beforeEach(() => {
+    window.URL.createObjectURL = jest.fn();
+  });
+
+  afterEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window.URL.createObjectURL as jest.Mock<any>).mockReset();
+  });
+
+  test("should download json file if it is invoked", () => {
+    downloadJsonFile(validFormattedJsonInput);
+    expect(window.URL.createObjectURL).toHaveBeenCalledTimes(1);
+  });
+
+  test("should download json file when button is clicked", () => {
+    const handleDownloadClick = () => downloadJsonFile(validFormattedJsonInput);
+    const { getByTestId } = render(<MockButton onClick={handleDownloadClick} />);
+    const button = getByTestId("mock-button");
+    button.click();
+    expect(window.URL.createObjectURL).toHaveBeenCalledTimes(1);
+  });
+});
